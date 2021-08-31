67c5cb55f86aba68ec0afb96110e2f660a51ce54
Ignacio Curto <ignacio.n.curto@gmail.com>
2021-08-31 12:45:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Clase 1:
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Curto - Proyecto entregable 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/IgnacioCurto/React-CoderHouse"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Repositorio de GitHub
         </a>
       </header>
     </div>
