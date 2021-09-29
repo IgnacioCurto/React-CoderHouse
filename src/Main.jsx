3ae7397ac8209e9b4f0f8cb1ad1f54a03cf04613
Ignacio Curto <ignacio.n.curto@gmail.com>
2021-09-29 12:46:58 -0300

    item List

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,12 +1,11 @@
+import ItemList from './components/itemListContainer';
 
 
 const main = () => {
     return ( 
         <main>
             <h2 class="mt-4 text-center" style={{color: "white", fontFamily:"'Lato', sans-serif"}}>Inicio de página</h2>
-            <div style={{width:"100%", height:"35rem", marginTop:"2rem", backgroundColor: "grey"}}>
-                {/* placeholder area */}
-            </div>
+            <ItemList/>
         </main>
     );
 }
---
+++ src/components/itemListContainer.jsx
@@ -0,0 +1,9 @@
+const ItemList = () => {
+    return ( 
+        <div style={{width:"100%", height:"35rem", marginTop:"2rem", backgroundColor: "grey"}}>
+            {/* placeholder area */}
+        </div>
+    );
+}
+
+export default ItemList;
